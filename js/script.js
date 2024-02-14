da4c689cb4da3963c9aeb3af260b93a25da9f52b
Daniel Hanák <d.hanak@quadient.com>
2024-02-14 20:12:39 +0100

    added smooth scrolling via js

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ const buttonMobileNavigationEl = document.querySelector(".btn-mobile-nav");
 buttonMobileNavigationEl.addEventListener("click", function () {
     headerEl.classList.toggle("nav-open");
 })
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
+
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior: "smooth"});
+        }
+
+        if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
+        }
+    })
+})
